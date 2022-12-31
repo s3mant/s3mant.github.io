8a5f03fb344701d17c84a959de2a27ca881e1119
Bad Boy <sonupanda078@gmail.com>
2022-12-31 16:26:26 +0530

    amongus?

--- public/js/accounts.js
+++ public/js/accounts.js
@@ -1,1 +1,6 @@
-export default { github: "https://github.com/s3mant/" };
+export default {
+  discord: "https://discord.gg/8YDCB87E5r",
+  insta: "https://www.instagram.com/its.semant",
+  instagram: "https://www.instagram.com/its.semant",
+  github: "https://github.com/s3mant",
+};
